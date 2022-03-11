52f3683538ca2239b9d66c2b5ae0a884726f1d5c
keddib <keddib@student.1337.ma>
2022-03-11 01:31:02 +0100

    typedef allocator

--- utils/allocator.hpp
+++ utils/allocator.hpp
@@ -3,41 +3,35 @@
 
 #include <cstdlib>
 
-template<typename T>
-class Simple_alloc
-{ // use new[] and delete[] to allocate and deallocate bytes
-	public:
-		typedef size_t            size_type;
-		typedef ptrdiff_t         difference_type;
-		typedef T*              pointer;
-		typedef const T*        const_pointer;
-		typedef T&              reference;
-		typedef const T&        const_reference;
-		typedef T               value_type;
-		typedef std::true_type propagate_on_container_move_assignment;
-		typedef std::true_type is_always_equal;
-		Simple_alloc() {
-		}
-		T* allocate(size_t n) {
-			 return (T *)std::malloc( n * sizeof(T) );
-		}
+namespace ft{
 
-		void construct (T* p, const T val) {
-			p = new((void *)p) T(val);
-		}
+	template<typename T>
+	class Simple_alloc
+	{
+		// use std::malloc() and std::free() to allocate and deallocate bytes
+		public:
+			//types
+			typedef size_t				size_type;
+			typedef ptrdiff_t			difference_type;
+			typedef T*					pointer;
+			typedef const T*			const_pointer;
+			typedef T&					reference;
+			typedef const T&			const_reference;
+			typedef T					value_type;
 
-		void destroy(T *p)
-		{
-			 ((T*)p)->~T();
-		}
+			// member func
+			Simple_alloc() {}
 
-		void deallocate(T* p, size_t n) {
-			free(p);
-		}
-		size_type max_size() const
-		{
-			return std::numeric_limits<size_type>::max() / sizeof(value_type);
-		}
-};
+			T* allocate(size_t n) { return (T *)std::malloc( n * sizeof(T) ); }
+
+			void construct (T* p, const T val = T()) { p = new((void *)p) T(val); }
+
+			void destroy(T *p) { ((T*)p)->~T(); }
+
+			void deallocate(T* p, size_t n) { std::free(p); }
+
+			size_type max_size() const { return std::numeric_limits<size_type>::max() / sizeof(value_type); }
+	};
+} // ft
 
 #endif
