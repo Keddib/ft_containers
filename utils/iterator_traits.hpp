1f76c6cf9a6c4645b46ad1ccbda7f47c2f78ab13
keddib <keddib@student.1337.ma>
2022-03-16 12:11:12 +0100

    iterator traits modification

--- utils/iterator_traits.hpp
+++ utils/iterator_traits.hpp
@@ -1,41 +1,37 @@
 #ifndef TF_ITERATOR_TRAITS
 #define TF_ITERATOR_TRAITS
-#include <iterator>
 
+#include <iterator>
 
 namespace ft {
 
-	template <typename Iterator>
-	class iterator_traits {
-		public:
-			//types
-			typedef typename Iterator::difference_type		difference_type;
-			typedef typename Iterator::value_type			value_type;
-			typedef typename Iterator::pointer				pointer;
-			typedef typename Iterator::reference			reference;
-			typedef typename Iterator::iterator_category	iterator_category;
+	template<class Iter>
+	struct iterator_traits
+	{
+		typedef typename Iter::difference_type difference_type;
+		typedef typename Iter::value_type value_type;
+		typedef typename Iter::pointer pointer;
+		typedef typename Iter::reference reference;
+		typedef typename Iter::iterator_category iterator_category;
+	};
+
+	template<class T>
+	struct iterator_traits<T*>
+	{
+		typedef ptrdiff_t difference_type;
+		typedef T value_type;
+		typedef T* pointer;
+		typedef T& reference;
+		typedef std::random_access_iterator_tag iterator_category;
 	};
 
 	template <typename T>
-	class iterator_traits<T*> {
-		public:
-			//types
-			typedef ptrdiff_t								difference_type;
-			typedef T										value_type;
-			typedef T*										pointer;
-			typedef T&										reference;
-			typedef std::random_access_iterator_tag			iterator_category;
-	};
-
-	template <typename T>
-	class iterator_traits<const T*> {
-		public:
-			//types
-			typedef ptrdiff_t								difference_type;
-			typedef T										value_type;
-			typedef const T*								pointer;
-			typedef const T&								reference;
-			typedef std::random_access_iterator_tag			iterator_category;
+	struct iterator_traits<const T*> {
+		typedef ptrdiff_t								difference_type;
+		typedef T										value_type;
+		typedef const T*								pointer;
+		typedef const T&								reference;
+		typedef std::random_access_iterator_tag			iterator_category;
 	};
 
 } // ft
