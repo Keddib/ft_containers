f06cf599e637e0950fdfa6440fa6dfce7c171e4d
keddib <keddib@student.1337.ma>
2022-04-21 02:48:35 +0000

    add allocator to RBT add delete

--- map/RBT.hpp
+++ map/RBT.hpp
@@ -4,19 +4,22 @@
 #define BLACK 0
 #define RED 1
 
+
+#include <memory>
+
 namespace ft {
 
 // node struct
 template <typename _Tp >
 struct node {
 
-	_Tp _M_Data;
-	bool _color;
-	node *_p; //parent
-	node *_left; // left child
-	node *_right; // right child;
+	_Tp value;
+	bool color;
+	node *p; //parent
+	node *left; // left child
+	node *right; // right child;
 	node (const _Tp &x = _Tp(), node *par = NULL, node *lef = NULL, node *ri = NULL, bool col = BLACK)
-	: _M_Data(x), _p(par), _left(lef), _right(ri), _color(col) {}
+	: value(x), p(par), left(lef), right(ri), color(col) {}
 };
 
 
@@ -35,77 +38,178 @@ class _RBT_base
 	//
 
 		typedef typename _Alloc::template rebind<node<_Tp> >::other	_Node_Alloc_type;
+		typedef typename _Node_Alloc_type::size_type				size_type;
+		typedef node<_Tp>											Node;
 
 		_Node_Alloc_type _N_Alloc;
 
-		node<_Tp>* _M_get_node()
+		Node* _M_get_node()
 		{ return _N_Alloc.allocate(1); }
 
-		void	_M_put_node(node<_Tp>* __p)
+		void	_M_put_node(Node* __p)
 		{ _N_Alloc.deallocate(__p, 1); }
 
-	public:
+	protected:
 		typedef _Alloc allocator_type;
 
-		allocator_type get_allocator() const
-		{ return allocator_type( _Node_Alloc_type() ); }
+		allocator_type get_allocator() const { return allocator_type(); }
+		size_type max_size() const { return _N_Alloc.max_size() ; }
 
 		_RBT_base(const allocator_type& __a) : _N_Alloc(__a) { }
 
-		// This is what actually destroys the list.
 		~_RBT_base() {}
 
+		Node* _create_node(const _Tp& __x = _Tp())
+		{
+			Node* p = this->_M_get_node();
+			try {
+				_N_Alloc.construct(p, Node(__x));
+			}
+			catch(...) {
+				_M_put_node(p);
+				// __throw_exception_again;
+			}
+			return p;
+		}
+		void _delete_node(Node *p)
+		{
+			try {
+				_N_Alloc.destroy(p);
+			}
+			catch(...) {
+				// __throw_exception_again;
+			}
+			_M_put_node(p);
+		}
 };
 
 
-template <typename T, typename Compare>
-class RBT {
+template <typename T, typename Compare, typename Alloc = std::allocator<T> >
+class RBT : protected _RBT_base<T, Alloc> {
 
 	public: // types
-		typedef	T											value_type;
-		typedef Compare										key_compare;
-		typedef node<T>										Node;
+		typedef	T												value_type;
+		typedef	typename T::first_type							Key;
+		typedef	typename T::second_type							Value;
+		typedef Compare											key_compare;
+		typedef node<T>											Node;
+		typedef _RBT_base<T, Alloc>								Base;
+		typedef typename Base::size_type						size_type;
+		// typedef _RBT_base<T, Alloc>::_Node_Alloc_type			node_alloc_type;
 
 	private: // atributes
 		Node *_NIL;
 		Node *_root;
+		key_compare _Com;
+		Alloc _A;
+		size_t	_size;
 
 	public:
-		RBT(Node *nil):_NIL(nil), _root(_NIL) {}
+		RBT(const key_compare &c = key_compare(), const Alloc &a = Alloc()): _Com(c), _A(a), Base(_A){
+			_NIL = this->_create_node();
+			_root = _NIL;
+			_size = 0;
+		}
 
-		void insert(Node *z) { // check duplication
+		~RBT() {
+			clear(_root);
+			this->_delete_node(_NIL);
+			_root = NULL;
+			_NIL = NULL;
+		}
+
+		std::pair<Node*, bool> insert(const value_type &x) {
+			Node *z = this->_create_node(x);
+			return insert(z);
+		}
+
+		std::pair<Node*, bool> insert(Node *z) { // check duplication
 			Node *y = _NIL;
 			Node *x = _root;
 			while (x != _NIL) {
 				y = x;
-				if  (z->_M_Data.first < x->_M_Data.first) // <
+				if  (_Com(z->value.first, x->value.first)) // <
 					x = x->left;
-				else x = x->right; // >
+				else if (_Com(x->value.first, z->value.first))
+					x = x->right;
+				else {
+					this->_delete_node(z);
+					return std::make_pair(x, false);
+				}
 			}
 			z->p = y;
 			if (y == _NIL)
 				_root =z;
-			else if(z->_M_Data.first < y->_M_Data.first)
+			else if(z->value.first < y->value.first)
 				y->left = z;
 			else y->right = z;
 			z->left = _NIL;
 			z->right = _NIL;
 			z->color = RED;
 			insertFIXUP(z);
+			_size++;
+			return std::make_pair(z, true);
 		}
-		void deleteNode(const_reference value) {}
 
-		Node *searchTree(const_reference key) const
+		int deleteNode(const Key &key) {
+			Node *z = search(key);
+			if (z != _NIL) {
+				deleteNode(z);
+				_size--;
+				return 1;
+			}
+			return 0;
+		}
+
+		void deleteNode(Node *z) {
+			Node *y = z;
+			Node *x;
+			bool OriginalColor = y->color;
+			if (z->left == _NIL) {
+				x = z->right;
+				TRANSPLANT(z, z->right);
+			}
+			else if (z->right == _NIL) {
+				z = z->left;
+				TRANSPLANT(z, z->left);
+			} else {
+				y = minimum(z->right);
+				OriginalColor = y->color;
+				x = y->right;
+				if (y->p == z)
+					x->p = y;
+				else {
+					TRANSPLANT(y , y->right);
+					y->right = z->right;
+					y->right->p = y;
+				}
+				TRANSPLANT(z, y);
+				y->left = z->left;
+				y->left->p = y;
+				y->color = z->color;
+			}
+			this->_delete_node(z);
+			if (OriginalColor == BLACK)
+				deleteFIXUP(x);
+		}
+
+		Node *search(const Key &key) const
 		{
 			Node *tmp = _root;
-			while ( tmp != _NIL && tmp->_M_Data.first != key)
+			while ( tmp != _NIL )
 			{
-				if (key < tmp->key)
+				if (_Com(key, tmp->value.first))
 					tmp = tmp->left;
-				else
+				else if (_Com(tmp->value.first, key))
 					tmp = tmp->right;
+				else
+					return _NIL;
 			}
 			return tmp;
+		}
+
+		Node *minimum() const {
+			return minimum(_root);
 		}
 
 		Node *minimum(Node *t) const
@@ -113,9 +217,13 @@ class RBT {
 			if (t == _NIL)
 				return t;
 			Node *tmp = t;
-			while (tmp->_M_Data.first != _NIL)
-				tmp = tmp->_M_Data.first;
+			while (tmp->left != _NIL)
+				tmp = tmp->left;
 			return tmp;
+		}
+
+		Node *maximum() const {
+			return maximum(_root);
 		}
 
 		Node *maximum(Node *t) const
@@ -154,6 +262,22 @@ class RBT {
 			return y;
 		}
 
+		void clear() {
+			clear(_root);
+			_root = _NIL;
+			_size = 0;
+		}
+
+		const Node *nil() const { return	_NIL; }
+
+		const Node *root() const { return	_root; }
+
+		size_type	max_size() const { return Base::max_size(); }
+
+		size_t size() const { return _size; }
+
+		key_compare  key_comp() { return _Com; }
+
 		void printBT(const std::string& prefix, const Node * root, bool isLeft)
 		{
 		    if( root != _NIL )
@@ -161,7 +285,7 @@ class RBT {
 		        std::cout << prefix;
 		        std::cout << (isLeft ? "├──" : "└──" );
 		        // print the value of the node
-		        std::cout << root->_M_Data.first << ':' << (root->color ? 'R' : 'B') << std::endl;
+		        std::cout << root->value.first << ':' << (root->color ? 'R' : 'B') << std::endl;
 		        // enter the next tree level - left and right branch
 		        printBT( prefix + (isLeft ? "│   " : "    "), root->left, true);
 		        printBT( prefix + (isLeft ? "│   " : "    "), root->right, false);
@@ -174,6 +298,27 @@ class RBT {
 		}
 
 	private: // mem functions
+
+		void clear(Node* node)
+		{
+			if (node == _NIL) return;
+
+			/* first delete both subtrees */
+			clear(node->left);
+			clear(node->right);
+			/* then delete the node */
+			this->_delete_node( node );
+		}
+
+		void TRANSPLANT(Node *u, Node *v) {
+			if (u->p == _NIL)
+				_root = v;
+			else if (u->p->left == u)
+				u->p->left = v;
+			else
+				u->p->right = v;
+			v->p = u->p;
+		}
 
 		void LEFT_ROTATE(Node *x)
 		{
@@ -251,18 +396,67 @@ class RBT {
 			}
 			_root->color = BLACK;
 		}
+		void deleteFIXUP(Node *x) {
 
-		Node* _M_create_node(const value_type& __x = value_type())
-		{
-			_Node* __p = this->_M_get_node();
-			try {
-				std::_N_Alloc.construct(&__p->_M_data, __x);
+			while (x != _root and x->color == BLACK)
+			{
+				if (x == x->p->left) {
+					Node *w = x->p->right;
+					if (w->color == RED) {
+						w->color = BLACK;
+						x->p->color = RED;
+						LEFT_ROTATE(x->p);
+						w = x->p->right;
+					}
+					if (w->left->color == BLACK && w->right->color == BLACK) {
+						w->color = RED;
+						x = x->p;
+					}
+					else {
+
+						if (w->right->color == BLACK) {
+							w->left->color = BLACK;
+							w->color = RED;
+							RIGHT_ROTATE(w);
+							w = x->p->right;
+						}
+						w->color = x->p->color;
+						x->p->color = BLACK;
+						w->right->color = BLACK;
+						LEFT_ROTATE(x->p);
+						x = _root;
+					}
+				}
+				else {
+					// (same as then clause with “right” and “left” exchanged)
+					Node *w = x->p->left;
+					if (w->color == RED) {
+						w->color = BLACK;
+						x->p->color = RED;
+						LEFT_ROTATE(x->p);
+						w = x->p->left;
+					}
+					if (w->right->color == BLACK && w->left->color == BLACK) {
+						w->color = RED;
+						x = x->p;
+					}
+					else {
+
+						if (w->left->color == BLACK) {
+							w->right->color = BLACK;
+							w->color = RED;
+							RIGHT_ROTATE(w);
+							w = x->p->left;
+						}
+						w->color = x->p->color;
+						x->p->color = BLACK;
+						w->left->color = BLACK;
+						LEFT_ROTATE(x->p);
+						x = _root;
+					}
+				}
 			}
-			catch(...) {
-				_M_put_node(__p);
-				__throw_exception_again;
-			}
-			return __p;
+			x->color = BLACK;
 		}
 };
 
